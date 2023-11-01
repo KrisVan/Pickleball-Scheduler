16453aa49dab2aff60c5f1221690f978c8597eab
Kris Van <kristophervan1999@gmail.com>
2023-10-31 22:02:14 -0400

    Added test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-/* Test Example
-test('renders learn react link', () => {
+
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText('Cities');
+  const linkElement = screen.getByText('Pickleball Scheduler');
   expect(linkElement).toBeInTheDocument();
 });
-*/
