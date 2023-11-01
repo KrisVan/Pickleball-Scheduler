7ef6be7f8a1c5be80427da4037c6eb9336fdbc10
Kris Van <kristophervan1999@gmail.com>
2023-10-31 21:56:04 -0400

    Removed unused test and required babel dependency

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+/* Test Example
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText('Cities');
   expect(linkElement).toBeInTheDocument();
 });
+*/
