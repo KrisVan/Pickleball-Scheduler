b3690f12445543714e4e005e470c56c6d112268d
Kris Van <kristophervan1999@gmail.com>
2023-11-05 11:54:31 -0500

    Changed title and added title test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,29 @@
 // @ts-check
+// To run test: npx playwright test
+
 const { test, expect } = require('@playwright/test');
 
+// Test if the page has the expected title.
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.goto('http://localhost:3000/Pickleball-Scheduler');
+  await expect(page).toHaveTitle(/Pickleball Scheduler/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+// Sample test
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
