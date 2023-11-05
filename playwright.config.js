750c88b7662c611429ee3c1801e6eebe739ed01f
Kris Van <kristophervan1999@gmail.com>
2023-11-05 13:38:19 -0500

    Added reporter for circleci

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
+const { getEnvironmentData } = require('worker_threads');
 
 /**
  * Read environment variables from file.
@@ -21,7 +22,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? [ ['junit', { outputFile: 'results.xml' }] ] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
