9c1005e1e7698fc578b4ccea955da49f8a9f69b3
Andrew Reid <atreid@gmail.com>
2023-11-20 17:08:43 -0500

    add linting and lint step

--- packages/server/src/pickle-ball-service.js
+++ packages/server/src/pickle-ball-service.js
@@ -3,23 +3,23 @@ import pino from 'pino';
 import { apisRoute } from './apis-route.js';
 
 export class PickleballService {
-    constructor() {
-        this.pino = pino();
-    }
+  constructor() {
+    this.pino = pino();
+  }
 
-    async start(port) {
-        const fastify = Fastify({
-            logger: true
-        });
+  async start(port) {
+    const fastify = Fastify({
+      logger: true,
+    });
 
-        fastify.register(apisRoute);
+    fastify.register(apisRoute);
 
-        return fastify.listen({ port }, (err, address) => {
-            if (err) {
-                fastify.log.error(err);
-                process.exit(1);
-            }
-            this.pino.info(`server listening on ${address}`);
-        });
-    }
+    return fastify.listen({ port }, (err, address) => {
+      if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+      }
+      this.pino.info(`server listening on ${address}`);
+    });
+  }
 }
