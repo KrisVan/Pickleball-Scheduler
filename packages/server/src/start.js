958d5322ff98fbf55e7c2c438ace394e3bb7e043
Kris Van <kristophervan1999@gmail.com>
2024-01-08 10:37:42 -0500

    Prisma seed script added

--- packages/server/src/start.js
+++ packages/server/src/start.js
@@ -1,8 +1,11 @@
 import pino from 'pino';
+import { PrismaClient } from '@prisma/client';
 import { PickleballService } from './pickle-ball-service.js';
 
 const logger = pino();
+//const prisma = new PrismaClient({ errorFormat: 'minimal' });
 
+// Start server at port 5000
 export const start = async (port = 5000) => {
   logger.info('Starting PickleballService');
   const service = new PickleballService();
@@ -10,4 +13,6 @@ export const start = async (port = 5000) => {
   return server;
 };
 
+// Execute start server
+console.log('Start server');
 start();
