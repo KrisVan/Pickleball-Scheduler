767145165313d20b8752790527aa648a952d8684
Kris Van <kristophervan1999@gmail.com>
2024-01-11 16:16:45 -0500

    Added pages organization. Non functional.

---
+++ packages/web/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Header } from '../components/Header/Header';
+import { PickleballCalendar } from '../components/PickleballCalendar/PickleballCalendar';
+import '../App.css';
+
+const queryClient = new QueryClient();
+
+export default function Home() {
+    return(
+        <div>
+            <QueryClientProvider client={queryClient}>
+                <Header />
+                <PickleballCalendar />
+            </QueryClientProvider>
+        </div>
+        
+    )
+}
