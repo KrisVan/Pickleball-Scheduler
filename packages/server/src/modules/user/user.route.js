310b32641b1bc464546a933104fb6edf9553e11c
Kris Van <kristophervan1999@gmail.com>
2024-01-17 15:26:44 -0500

    Defined user routes

---
+++ packages/server/src/modules/user/user.route.js
@@ -0,0 +1,41 @@
+import { createUser } from './user.controller.js'
+import { $ref } from './user.schema.js';
+
+export async function userRoutes(app) {
+  app.get('/', (req, reply) => {
+    reply.send({ message: '/users route hit' });
+  });
+
+  // Register
+  app.post(
+    '/register',
+    {
+      schema: {
+        body: $ref('createUserSchema'),
+        response: {
+          201: $ref('createUserResponseSchema'),
+        },
+      },
+    },
+    createUser,
+  );
+
+  // Login
+  app.post(
+    '/login',
+    {
+      schema: {
+        body: $ref('loginSchema'),
+        response: {
+          201: $ref('loginResponseSchema'),
+        },
+      },
+    },
+    () => {},
+  );
+
+  // Logout
+  app.delete('/logout', () => {});
+
+  app.log.info('user routes registered');
+}
