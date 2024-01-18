1e557381e805c32f25e7907346db71925350fc69
Kris Van <kristophervan1999@gmail.com>
2024-01-18 12:04:16 -0500

    Added authenticate decoration to Fastify

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -9,38 +9,6 @@ import { userSchemas } from './modules/user/user.schema.js';
 const app = Fastify({ logger: true });
 const logger = pino();
 
-// Register routes
-app.register(userRoutes, { prefix: 'api/users' });
-
-// Add schemas to Fastify
-Object.values(userSchemas).forEach((schema) => {
-  app.addSchema(schema);
-});
-
-// Register fjwt
-app.register(fjwt, { secret: process.env.supersecretcode });
-
-// Hook to pass token to req obj
-app.addHook('preHandler', (req, res, next) => {
-  req.jwt = app.jwt;
-  return next();
-});
-
-// Cookies
-app.register(fCookie, {
-  secret: 'some-secret-key',
-  hook: 'preHandler',
-});
-
-// Graceful shutdown
-const listeners = ['SIGINT', 'SIGTERM'];
-listeners.forEach((signal) => {
-  process.on(signal, async () => {
-    await app.close();
-    process.exit(0);
-  });
-});
-
 // Start server at port
 export const start = async (port = 5000, host = '0.0.0.0') => {
   logger.info('Starting PickleballService');
@@ -58,6 +26,55 @@ export const start = async (port = 5000, host = '0.0.0.0') => {
     },
   );
 };
+//----------------------------------------------------------------
+// Register routes
+app.register(userRoutes, { prefix: 'api/users' });
 
+// Add schemas to Fastify
+Object.values(userSchemas).forEach((schema) => {
+  app.addSchema(schema);
+});
+
+// Register fjwt
+app.register(fjwt, { secret: process.env.supersecretcode });
+
+// Hook to pass token to req obj as prehandler before route calls
+app.addHook('preHandler', (req, res, next) => {
+  req.jwt = app.jwt;
+  return next();
+});
+
+// Register cookie to fastify
+app.register(fCookie, {
+  secret: 'some-secret-key',
+  hook: 'preHandler',
+});
+
+// Authenticate
+app.decorate(
+  'authenticate',
+  async (req, reply) => {
+    const token = req.cookies.access_token
+    // If no token, user not authenticated
+    if (!token) {
+      return reply.status(401).send({ message: 'Authentication required' })
+    }
+    // Verify token with jwt
+    const decoded = req.jwt.verify(token)
+    // Attach current user payload to req
+    req.user = decoded
+  },
+)
+
+// Graceful shutdown
+const listeners = ['SIGINT', 'SIGTERM'];
+listeners.forEach((signal) => {
+  process.on(signal, async () => {
+    await app.close();
+    process.exit(0);
+  });
+});
+
+// Start server
 console.log('Starting server');
 start();
