9f0a0ea9c8e0319eaeec8a9dff44acef32d209d1
Kris Van <kristophervan1999@gmail.com>
2024-01-21 22:32:07 -0500

    Removed placeholder components

--- packages/web/src/pages/Scheduler.jsx
+++ packages/web/src/pages/Scheduler.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Header } from '../components/Header/Header';
 import { PickleballCalendar } from '../components/PickleballCalendar/PickleballCalendar';
 
 const queryClient = new QueryClient();
@@ -10,7 +9,6 @@ export default function Scheduler() {
         <div>
             <QueryClientProvider client={queryClient}>
                 <h2>Scheduler</h2>
-                <Header />
                 <PickleballCalendar />
             </QueryClientProvider>
         </div>
