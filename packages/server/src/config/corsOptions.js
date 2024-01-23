ca679b413ccebe318eb7db849d2c47d59e9b2fe2
Kris Van <kristophervan1999@gmail.com>
2024-01-23 16:15:18 -0500

    Added middleware to verify if access token is valid

--- packages/server/src/config/corsOptions.js
+++ packages/server/src/config/corsOptions.js
@@ -1,17 +1,17 @@
 const whitelist = [
   'https://www.yoursite.com',
   'http://0.0.0.0:5000',
-  'http://localhost:3000'
+  'http://localhost:3000',
 ];
 
 export const corsOptions = {
   origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-          callback(null, true)
-      } else {
-          callback(new Error('Not allowed by CORS'));
-      }
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   },
   credentials: true,
   optionsSuccessStatus: 200,
-}
+};
---
+++ packages/server/src/middleware/verifyJWT.js
@@ -0,0 +1,18 @@
+// Verifies if access token is valid
+export const verifyJWT = (req, reply, next) => {
+  const authHeader = req.headers.authorization; // "Bearer tokenString"
+  if (!authHeader) return reply.status(401).send({ message: 'Authentication required' });
+  const token = authHeader.split(' ')[1];
+  console.log(token);
+  req.jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET,
+    (err, decoded) => {
+      if (err) return reply.status(403).send({ message: 'Invalid token' }); // Invalid token
+      req.user = decoded.username;
+      next();
+      return reply.status(200);
+    },
+  );
+  return reply.status(200);
+};
