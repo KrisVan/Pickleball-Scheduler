9f3f9324d0a79f0775c0538b047891544ad01916
KrisVan <kristophervan1999@gmail.com>
2024-02-13 10:32:54 -0500

    Landing is rendered when not logged in at home page

---
+++ packages/web/src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Box from '@mui/system/Box';
+import { Typography } from '@mui/material';
+import Copyright from '../components/Copyright/Copyright.jsx'
+
+
+export default function Landing() {
+  return (
+    <Box>
+      <Typography>Landing</Typography>
+      <Copyright />
+    </Box>
+  )
+}
