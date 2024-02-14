77fbc54c960bf6bd8eef982981f477b29d95a06c
KrisVan <kristophervan1999@gmail.com>
2024-02-13 22:56:22 -0500

    Added Hero to describe what app is and promt sign up

---
+++ packages/web/src/components/Hero/Hero.jsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import { alpha } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom'
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+export default function Hero() {
+  return (
+    <Box
+      id="hero"
+      sx={{
+        width: '100%',
+        backgroundSize: '100% 20%',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 },
+        }}
+      >
+        <Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
+          <Typography
+            component="h1"
+            variant="h1"
+            sx={{
+              display: 'flex',
+              flexDirection: { xs: 'column', md: 'row' },
+              alignSelf: 'center',
+              textAlign: 'center',
+            }}
+          >
+            Schedule Sessions Together
+          </Typography>
+          <Typography variant="body1" textAlign="center" color="text.secondary">
+            Effortlessly organize and share sessions on a collaborative calendar.<br />
+            See who's available, coordinate events, and never miss a beat with our intuitive interface
+          </Typography>
+          <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            alignSelf="center"
+            spacing={1}
+            useFlexGap
+            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to={`/register`}
+              key={'/register'}
+            >
+              Sign up
+            </Button>
+          </Stack>
+        </Stack>
+        <Box
+          id="image"
+          sx={(theme) => ({
+            mt: { xs: 8, sm: 10 },
+            alignSelf: 'center',
+            height: { xs: 200, sm: 700 },
+            width: '100%',
+            backgroundImage:
+              theme.palette.mode === 'light'
+                // FIXME: Placeholder. Put in demo gifs
+                ? "url('')"
+                : "url('')",
+            backgroundSize: 'cover',
+            borderRadius: '10px',
+            outline: '1px solid',
+            outlineColor:
+              theme.palette.mode === 'light'
+                ? alpha('#BFCCD9', 0.5)
+                : alpha('#9CCCFC', 0.1),
+            boxShadow:
+              theme.palette.mode === 'light'
+                ? `0 0 12px 8px ${alpha('#9CCCFC', 0.2)}`
+                : `0 0 24px 12px ${alpha('#00ff00', 0.2)}`,
+          })}
+        />
+      </Container>
+    </Box>
+  );
+}
