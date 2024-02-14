d8dcad896d45eb47906bc48594d417e2c5b6783b
KrisVan <kristophervan1999@gmail.com>
2024-02-13 22:56:52 -0500

    Added landing page

--- packages/web/src/pages/Landing.jsx
+++ packages/web/src/pages/Landing.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Box from '@mui/system/Box';
-import { Typography } from '@mui/material';
-import Copyright from '../components/Copyright/Copyright.jsx'
+import Divider from '@mui/material/Divider';
+import Hero from '../components/Hero/Hero.jsx';
 
+import FeatureShowcase from '../components/FeatureShowcase/FeatureShowcase.jsx';
+import Footer from '../components/Footer/Footer.jsx';
 
 export default function Landing() {
   return (
-    <Box>
-      <Typography>Landing</Typography>
-      <Copyright />
+    <Box sx={{ bgcolor: 'background.default' }}>
+      <Hero />
+      <FeatureShowcase />
+      <Divider />
+      <Footer />
     </Box>
   )
 }
