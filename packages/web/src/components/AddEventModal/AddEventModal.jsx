373cf7341e454f532a4897f150e4e12c596ef98b
Kris Van <kristophervan1999@gmail.com>
2024-03-09 19:17:45 -0500

    Linting

--- packages/web/src/components/AddEventModal/AddEventModal.jsx
+++ packages/web/src/components/AddEventModal/AddEventModal.jsx
@@ -1,5 +1,4 @@
 
-import { useEffect } from "react"
 import { Link } from "react-router-dom"
 import {
   TextField,
--- packages/web/src/components/EditEventModal/EditEventModal.jsx
+++ packages/web/src/components/EditEventModal/EditEventModal.jsx
@@ -1,5 +1,4 @@
 
-import { useEffect } from "react"
 import {
   TextField,
   Dialog,
