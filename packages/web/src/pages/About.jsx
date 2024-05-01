c3bf458ee93d7aa7ec0ab61bab674bd8c82bcd55
KrisVan <kristophervan1999@gmail.com>
2024-05-01 00:14:47 -0400

    Added text, icon button links, and breakpoints for sizing

--- packages/web/src/pages/About.jsx
+++ packages/web/src/pages/About.jsx
@@ -2,64 +2,115 @@ import { Box } from '@mui/material';
 import { Container } from '@mui/material';
 import { Grid } from '@mui/material';
 import { Typography } from '@mui/material';
-
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Footer from '../components/Footer/Footer.jsx';
 
 export default function About() {
-  const bio = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  const bio = "I'm a developer with experience in full-stack web development and machine learning. I've worked on collaborative scheduling apps and built machine learning models for Amazon review classification. Proficient in JavaScript, Java, Python, and frameworks like ReactJS and Express, I enjoy solving complex problems and learning new technologies. I hold a degree in computer science from George Mason University, where I gained a solid foundation in both theory and practice. I'm dedicated to delivering high-quality solutions and continually improving my skills."
 
 	return(
     <>
       <Container
-        // maxWidth="lg"
         sx={{
-          mt: { xs: 8, sm: 10 }, 
-          mx:{ xs: 2, sm: 5 },
+          my: { xs: 2, sm: 4, md: 6 },
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'left',
         }}
       >
         <Typography
           component="h1"
-          variant="h1"
+          variant="h3"
           color="inherit"
           noWrap
-          sx={{ flexGrow: 1 }}
+          sx={{ flexGrow: 1, mb:1}}
         >
           About
         </Typography>
-        <Grid
-          container
+        <Grid container
           direction="row"
           wrap='nowrap'
-          spacing={10}
+          columnSpacing={{ xs: 2, sm: 4, md: 4 }}
           justifyContent="space-evenly"
           alignItems="flex-start"
         >
-          <Grid item xs={false}>
+          <Grid item xs={false} md={6}>
             <Typography
               component="body1"
-              variant="h4"
-              color="inherit"
+              variant="body1"
+              color="text.secondary"
               paragraph={true}
               noWrap={false}
-              sx={{ flexGrow: 1 }}
             >
               {bio}
             </Typography>
           </Grid>
-          <Grid item xs={100}>
+          <Grid item xs={6} md={7}>
             <Box
               id="image"
               sx={{
-                mt: { xs: 4, sm: 1 },
                 alignSelf: 'center',
-                height: { xs: 200, sm: 700 },
+                height: 550,
                 width: '100%',
+                minWidth: 300,
+                mb: 1,
                 backgroundRepeat: "no-repeat",
                 backgroundPosition: "center center",
+                backgroundOrigin: "center center",
                 backgroundImage: 'url("images/krisProfile.png")',
                 backgroundSize: 'contained',
               }}
             />
+            <Stack
+              direction="row"
+              spacing={{ xs: 0, sm: 2, md: 4 }}
+              useFlexGap
+              flexWrap="wrap"
+              sx={{
+                color: 'text.secondary',
+              }}
+              
+            >
+              <IconButton
+                color="inherit"
+                href="https://github.com/KrisVan/"
+                target={"_blank"}
+                aria-label="GitHub Profile"
+              >
+                <GitHubIcon fontSize="medium"/>
+                <Typography
+                  color="inherit"
+                  variant="body1"
+                  component="subtitle"
+                  align='center'
+                  mx={1}
+                >
+                  GitHub
+                </Typography>
+              </IconButton>
+              <IconButton
+                color="inherit"
+                href="https://www.linkedin.com/in/kristopher-van-71b339274/"
+                target={"_blank"}
+                aria-label="LinkedIn Profile"
+                sx={{ alignSelf: 'center'}}
+              >
+                <LinkedInIcon fontSize="medium"/>
+                <Typography
+                  color="inherit"
+                  variant="body1"
+                  component="subtitle"
+                  align='center'
+                  mx={1}
+                >
+                  LinkedIn
+                </Typography>
+              </IconButton>
+            </Stack>
+            
           </Grid>
         </Grid>
       </Container>
